d774de99524513dee006f9646069c6a0e078b9f4
alfredo <callizayagutierrezalfredo@gmail.com>
2016-02-22 18:29:00 -0400

    cambios5

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,8 +29,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+#Definiendo mi app
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
