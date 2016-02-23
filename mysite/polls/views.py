53944829fb9ffc67505fc827ac626c15c8af31a8
alfredo <callizayagutierrezalfredo@gmail.com>
2016-02-23 01:13:09 -0400

    archivo estaticos

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render, get_object_or_404
-from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question
+from .models import Question, Choice
+from django.template import loader
+from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
-from .models import Choice
 from django.views import generic
+from django.utils import timezone
+from django.test import TestCase
+#Definiendo las vistas
+#def index(request):
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #context = {'latest_question_list': latest_question_list}
+    #return render(request, 'polls/index.html', context)
 
-#Creando las vistas
+#def detail(request, question_id):
+    #question = get_object_or_404(Question, pk=question_id)
+    #return render(request, 'polls/detail.html', {'question': question})
+
+#def results(request, question_id):
+    #question = get_object_or_404(Question, pk=question_id)
+    #return render(request, 'polls/results.html', {'question': question})
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -15,12 +28,18 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')
-
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
+        #return Question.objects.order_by('-pub_date')
+#Arriba se agrego por defecto para los 1ros 5 [:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Se excluyen todas las preguntas que no han sido seleccionadas.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -29,15 +48,38 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-    	#para verificar que los cambios se hacer solo atraves de una llamada POST
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "Debe elegir una opcion.",
+            'error_message': "Error debe elegir una opcion.",
         })
     else:
-
         selected_choice.votes += 1
         selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.',days=5)
+        response = self.client.get(reverse('polls:detail',args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.',days=-5)
+        response = self.client.get(reverse('polls:detail',args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text,status_code=200)
