cc80edb20fd226af3373cdffca25a6b17d38f45f
alfredo <callizayagutierrezalfredo@gmail.com>
2016-02-23 01:33:07 -0400

    personalizando sitio Administrativo

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,9 +13,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+
     def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 @python_2_unicode_compatible  # si solo es necesario para apoyar a python2
 class Choice(models.Model):
